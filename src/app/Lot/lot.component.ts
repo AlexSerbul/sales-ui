c5a77dc1d774c416a7fe00459dda407dc7d594aa
DogeKing2001 <serbaleks2001@gmail.com>
2021-10-19 04:41:45 +0300

    хеддер и лоты (в процессе)

---
+++ src/app/Lot/lot.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-lot',
+  templateUrl: './lot.component.html',
+  styleUrls: ['./lot.component.css']
+})
+export class LotComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LotComponent} from  './Lot/lot.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LotComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
